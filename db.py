a9fcf070ea33f3fc9c8d68f512da30312d3fdbf5
TetoDola <sudaravan10@icloud.com>
2025-02-14 15:27:47 +0100

    database structure update

--- db.py
+++ db.py
@@ -10,14 +10,23 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    memories = relationship("Memory", back_populates="user")
 # memory class
 class Memory(Base):
-    __tablename__ = "m"
+    __tablename__ = "memories"
     id = Column(Integer, primary_key=True, index=True)
-    #  Usage of memory type allows for easy addition of new memory types in the future
-    memory_type = Column(String, nullable=False)
-    memory = Column(Text, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    core_memory = Column(Text)
+    user_info_memory = Column(Text)
+    other_memory = Column(Text)
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    user = relationship("User", back_populates="memories")
+
+
 
 def init_db():
     Base.metadata.create_all(bind=engine)
